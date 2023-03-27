adf4b5b37bfaaea44992673b26cdbca59395ae19
Manubg <bgmanu2426@gmail.com>
2023-03-27 09:49:13 +0530

    video-44 completed

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,10 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+//Avilable routes
+app.use('/api/auth', require('./routes/auth'))
+// app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-    console.log(`Example app listening on http://localhost:${port}`)
+    console.log(`App listening on http://localhost:${port}`)
 })
