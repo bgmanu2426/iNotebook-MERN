aefa364de5d0a36503e9a2c7c19cab5be3f7fae8
Manubg <bgmanu2426@gmail.com>
2023-05-08 22:40:51 +0530

    backend updated

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectToMongo = async () => {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test')
+    await mongoose.connect('mongodb://127.0.0.1:27017/iNotebook')
         .then(() => {
             console.log("Database connected successfully");
         }).catch((err) => {
--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@ const connectToMongo = require('./db');
 const express = require('express')
 
 const app = express()
-const port = 3000
+const port = 5000
 connectToMongo();
 
 app.use(express.json())
 
 //Avilable routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+app.use('/api/auth', require('./routes/user.routes'))
+app.use('/api/notes', require('./routes/notes.routes'))
 
 app.listen(port, () => {
     console.log(`App listening on http://localhost:${port}`)
