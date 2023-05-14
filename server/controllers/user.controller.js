ea41cfe7f573cd13e3d7fe8561eea1235838836c
Manubg <bgmanu2426@gmail.com>
2023-05-14 21:50:57 +0530

    video - 50 completed

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,10 +1,15 @@
 const User = require('../models/user.model');
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 exports.createuser = async (req, res) => {
     try {
+        const { name, email, password } = req.body;
+
         // Check if the user with the email exists or not
-        let userExixts = await User.findOne({ email: req.body.email });
+        let userExixts = await User.findOne({ email });
         if (userExixts) {
             return res.status(400).send({ error: "Sorry! a user with email already exists try another" });
         }
@@ -12,18 +17,56 @@ exports.createuser = async (req, res) => {
         // Check for errors in validation
         const error = validationResult(req);
         if (error.isEmpty()) {
-            // Store the user data in database
-            const user = new User(req.body);
-            await user.save() // save in database
-                .then((result) => {
-                    res.status(200).send(result);
-                }).catch((err) => {
-                    res.status(400).json(err.message);
-                });
+            // Password salt generator with syncronus function
+            const salt = bcrypt.genSaltSync(10);
+            const securePass = bcrypt.hashSync(password, salt);
+
+            // Create a new user and save in DB
+            const user = await User.create({
+                name,
+                email,
+                password: securePass
+            });
+
+            const data = {
+                user: {
+                    id: user.id,
+                },
+            }
+            const authToken = jwt.sign(data, process.env.JWT_SECRET)
+            res.status(201).json({ authToken });
         } else {
             res.status(400).json({ errors: error.array() });
         }
     } catch (err) {
-        res.status(500).send(err.message);
+        console.log(err.message);
+        res.status(500).send("Internal server error");
+    }
+}
+
+exports.loginuser = async (req, res) => {
+    try {
+        // Check if the user with the email exists or not
+        const { email, password } = req.body;
+        let userExixts = await User.findOne({ email });
+        if (!userExixts) {
+            return res.status(400).send({ error: "Please login with vaild credentials" });
+        }
+
+        const passwordVerify = bcrypt.compareSync(password, userExixts.password);
+        if (!passwordVerify) {
+            return res.status(400).send({ error: "Please login with vaild credentials" });
+        }
+        const data = {
+            user: {
+                id: userExixts.id,
+            },
+        }
+
+        const authToken = jwt.sign(data, process.env.JWT_SECRET);
+        res.status(200).json({ authToken });
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send("Internal server error");
     }
 }
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,10 +2,16 @@ const userController = require('../controllers/user.controller');
 const { body, validationResult } = require('express-validator');
 
 module.exports = (app) => {
-    //Create a user using POST '/api/user/createuser'. Dosen't require authentication
+    //Create a user using POST at '/api/user/createuser'. Dosen't require authentication
     app.post('/api/user/createuser',
         body('email').isEmail().withMessage("Invalid Email"),
         body('name', "Name is invald or too short").isLength({ min: 4 }),
         body('password').isLength({ min: 6 }).withMessage("Password is too short")
         , userController.createuser);
+
+    //Authenticate user using POST at '/api/user/login'. Dosen't require authentication
+    app.post('/api/user/login',
+        body('email').isEmail().withMessage("Invalid Email"),
+        body('password').exists().withMessage("Password cannot be blank")
+        , userController.loginuser);
 }
