67eafed10dd69f44525d6840d65b0c307c416513
Manubg <bgmanu2426@gmail.com>
2023-05-26 18:42:22 +0530

    login and signup context added

--- client/src/components/Home/AddNote.jsx
+++ client/src/components/Home/AddNote.jsx
@@ -15,7 +15,8 @@ const AddNote = () => {
 
     const [note, addNote] = useState({ title: "", description: "", tag: "" })
     const [fetchNotes, setFetchNotes] = useState(false);
-    const handleOnClick = (e) => {
+
+    const handleSubmit = (e) => {
         e.preventDefault();
         addNotes(note.title, note.description, note.tag);
         setFetchNotes(true);
@@ -35,26 +36,59 @@ const AddNote = () => {
 
     return (
         <>
-            <form className="flex flex-col gap-3 w-1/2 m-auto">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-3 w-1/2 m-auto">
                 <div>
                     <div className="mb-2 block">
                         <Label htmlFor="title" value="Enter title" />
                     </div>
-                    <TextInput id="title" type="text" name='title' placeholder="Enter your title here" onChange={onChange} minLength={3} value={note.title} required={true} />
+                    <TextInput
+                        id="title"
+                        type="text"
+                        name='title'
+                        placeholder="Enter your title here"
+                        onChange={onChange}
+                        minLength={3}
+                        value={note.title}
+                        required={true}
+                    />
                 </div>
                 <div id="description">
                     <div className="mb-2 block">
                         <Label htmlFor="description" value="Enter description" />
                     </div>
-                    <Textarea id="description" name='description' required={true} onChange={onChange} minLength={7} value={note.description} rows={4} />
+                    <Textarea
+                        id="description"
+                        type="text"
+                        name='description'
+                        placeholder="Enter your description here"
+                        required={true}
+                        onChange={onChange}
+                        minLength={7}
+                        value={note.description}
+                        rows={4}
+                    />
                 </div>
                 <div>
                     <div className="mb-2 block">
                         <Label htmlFor="tag" value="Enter tag" />
                     </div>
-                    <TextInput id="tag" name='tag' type="text" onChange={onChange} value={note.tag} required={true} />
+                    <TextInput
+                        id="tag"
+                        name='tag'
+                        placeholder='Enter your tag here'
+                        type="text"
+                        onChange={onChange}
+                        value={note.tag}
+                        required={true}
+                    />
                 </div>
-                <Button color="dark" className='md:w-full w-[30%] mx-auto' disabled={note.title.length < 3 || note.description.length < 7} type="submit" onClick={handleOnClick}>Add Note</Button>
+                <Button
+                    color="dark"
+                    className='md:w-full w-[30%] mx-auto'
+                    type="submit"
+                >
+                    Add Note
+                </Button>
             </form>
         </>
     )
